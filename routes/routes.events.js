d844cc350298cf593afe769b57cf1b31952d51e6
utkarshsingh99 <utkarshsingh369@gmail.com>
2019-03-02 23:22:07 +0530

    Added 6th route

--- routes/routes.events.js
+++ routes/routes.events.js
@@ -5,4 +5,19 @@ router.get('/', (req, res) => {
     Events.find({}).then(data => res.send(data))
 })
 
+router.post('/teamsinfo', (req, res) => {
+    Events.findById(req.body.eventId)
+        .then(event => {
+            if(event) {
+                res.send(event)
+            } else {
+                res.send(404)           // Event Not Found
+            }
+        })
+})
+
+// router.post('/newteam', (req, res) => {
+//     Events.findByIdAndUpdate(req.body.eventId, {$})
+// })
+
 module.exports = router
