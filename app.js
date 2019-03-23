a3caf0551b25654256a3960021114ce319571d5b
utkarshsingh99 <utkarshsingh369@gmail.com>
2019-03-24 00:45:49 +0530

    Added upload limit

--- app.js
+++ app.js
@@ -21,8 +21,10 @@ mongoose.connect(keys.mongo.url, { useNewUrlParser: true, useCreateIndex: true }
 var app = express()
 
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json())
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
 
 app.use('/auth', authRoutes)
 app.use('/events', eventRoutes)
